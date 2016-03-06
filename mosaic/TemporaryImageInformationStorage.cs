8d5322b5e651eb4f8b329c9c731e24861fb522d9
pcieslik <czolgu@gmail.com>
2016-03-06 11:41:21 +0100

    added image information, temporary image and image information storages

---
+++ mosaic/TemporaryImageInformationStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mosaic
+{
+    internal interface ITemporaryImageInformationStorage
+    {
+        void Save(ImageInformation imageInformation);
+    }
+
+    internal sealed class TemporaryImageInformationStorage : ITemporaryImageInformationStorage
+    {
+        private string _filePath;
+
+        public TemporaryImageInformationStorage(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Save(ImageInformation imageInformation)
+        {
+        }
+    }
+}
---
+++ mosaic/TemporaryImageStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace mosaic
+{
+    internal interface ITemporaryImageStorage
+    {
+        void Save(Image image);
+    }
+
+    internal sealed class TemporaryImageStorage : ITemporaryImageStorage
+    {
+        public void Save(Image image)
+        {
+        }
+    }
+}
