7969daf81f04b87983649ecf3569c73b1a8231ff
pcieslik <czolgu@gmail.com>
2016-03-13 12:05:03 +0100

    renamed MainWindow to MosaicView

---
+++ mosaic.ui/MosaicView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace mosaic.ui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MosaicView : Window
+    {
+        public MosaicView()
+        {
+            InitializeComponent();
+        }
+    }
+}
