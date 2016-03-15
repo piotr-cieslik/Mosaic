5be87408a8a4c5de56fe3ad8ff4e49d01691d136
pcieslik <czolgu@gmail.com>
2016-03-15 19:34:10 +0100

    added event aggregator implementation

---
+++ EventAggregator/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventAggregator
+{
+    public sealed class Subscription<TMessage> where TMessage : IMessage
+    {
+        public Subscription(Action<TMessage> action)
+        {
+            Action = action;
+        }
+
+        public Action<TMessage> Action { get; }
+    }
+}
