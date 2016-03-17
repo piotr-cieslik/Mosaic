66e37eed77c960ec345ac8e8688bc093c442a04f
pcieslik <czolgu@gmail.com>
2016-03-17 20:41:41 +0100

    added progress notification view and view model

---
+++ mosaic.ui/ProgressNotification/ProgressNotificationView.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mosaic.ui.ProgressNotification
+{
+    public partial class ProgressNotificationView
+    {
+        public ProgressNotificationView()
+        {
+            InitializeComponent();
+            DataContext = new ProgressNotificationViewModel();
+        }
+    }
+}
---
+++ mosaic.ui/ProgressNotification/ProgressNotificationViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace mosaic.ui.ProgressNotification
+{
+    internal sealed class ProgressNotificationViewModel : INotifyPropertyChanged
+    {
+        private int _maximum;
+
+        private string _status;
+
+        private int _value;
+
+        public ProgressNotificationViewModel()
+        {
+            Status = ":(";
+            Maximum = 50;
+            Value = 10;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Maximum
+        {
+            get { return _maximum; }
+            set
+            {
+                _maximum = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+    }
+}
