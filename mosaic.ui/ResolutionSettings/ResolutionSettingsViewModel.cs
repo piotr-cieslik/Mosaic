6b71ea7d4a8f5caed58549f3ab4cbdba915f1c17
pcieslik <czolgu@gmail.com>
2016-04-02 10:28:43 +0200

    added new resolutions, added information about outpu image resolution

--- mosaic.ui/ResolutionSettings/ResolutionSettingsViewModel.cs
+++ mosaic.ui/ResolutionSettings/ResolutionSettingsViewModel.cs
@@ -1,6 +1,9 @@
-﻿using mosaic.ui.EventAggregation;
+﻿using mosaic.ui.BaseImageSelection;
+using mosaic.ui.EventAggregation;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
 
 namespace mosaic.ui.ResolutionSettings
 {
@@ -8,33 +11,41 @@ namespace mosaic.ui.ResolutionSettings
     {
         private readonly EventAggregator _eventAggregator;
 
+        private IEnumerable<ImageResolution> _imageResolutions;
+        private string _outputImageResolutionInfo;
         private ImageResolution _selectedImageResolution;
         private TileResolution _selectedTileResolution;
+        private IEnumerable<TileResolution> _tileResolutions;
 
         public ResolutionSettingsViewModel()
         {
             _eventAggregator = EventAggregatorProvider.GetInstance();
-
-            TileResolutions = new[]
-            {
-                new TileResolution(25),
-                new TileResolution(50),
-                new TileResolution(75),
-                new TileResolution(100)
-            };
-
-            ImageResolutions = new[]
-            {
-                new ImageResolution(160, 120),
-                new ImageResolution(320, 240),
-                new ImageResolution(120, 160),
-                new ImageResolution(240, 320)
-            };
+            _eventAggregator.Subscribe<BaseImageSelected>(OnBaseImageSelected);
+            _eventAggregator.Subscribe<ImageResolutionChanged>(x => GenerateOutputImageInfo());
+            _eventAggregator.Subscribe<TileResolutionChanged>(x => GenerateOutputImageInfo());
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public IEnumerable<ImageResolution> ImageResolutions { get; }
+        public IEnumerable<ImageResolution> ImageResolutions
+        {
+            get { return _imageResolutions; }
+            set
+            {
+                _imageResolutions = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        public string OutputImageResolutionInfo
+        {
+            get { return _outputImageResolutionInfo; }
+            set
+            {
+                _outputImageResolutionInfo = value;
+                PropertyChanged.Raise(this);
+            }
+        }
 
         public ImageResolution SelectedImageResolution
         {
@@ -66,6 +77,54 @@ namespace mosaic.ui.ResolutionSettings
             }
         }
 
-        public IEnumerable<TileResolution> TileResolutions { get; }
+        public IEnumerable<TileResolution> TileResolutions
+        {
+            get
+            {
+                return _tileResolutions;
+            }
+            set
+            {
+                _tileResolutions = value;
+                PropertyChanged.Raise(this);
+            }
+        }
+
+        private static ImageResolution[] CreateImageResolutions(BaseImageSelected message)
+        {
+            // it shouldn't be performed at UI thread
+            var image = Image.FromFile(message.Path);
+            var ratio = image.Width / (float)image.Height;
+            var numberOfTilesVertically = new[] { 50, 75, 100, 125, 150, 175, 200 };
+            return numberOfTilesVertically.Select(x => new ImageResolution((int)(x * ratio), x)).ToArray();
+        }
+
+        private static TileResolution[] CreateTileResolutions()
+        {
+            return new[]
+            {
+                new TileResolution(25),
+                new TileResolution(50),
+                new TileResolution(75),
+                new TileResolution(100)
+            };
+        }
+
+        private void GenerateOutputImageInfo()
+        {
+            var horizontalResolution = SelectedTileResolution.Resolution * SelectedImageResolution.NumberOfTilesHorizaontally;
+            var verticalResolution = SelectedTileResolution.Resolution * SelectedImageResolution.NumberOfTilesVertically;
+            var resolutionInMpx = horizontalResolution * verticalResolution / 1000000;
+            OutputImageResolutionInfo = string.Format("rozdzielczość obrazu wyjściowego: {0} x {1} ({2} Mpx)", horizontalResolution, verticalResolution, resolutionInMpx);
+        }
+
+        private void OnBaseImageSelected(BaseImageSelected message)
+        {
+            TileResolutions = CreateTileResolutions();
+            ImageResolutions = CreateImageResolutions(message);
+
+            SelectedTileResolution = TileResolutions.First();
+            SelectedImageResolution = ImageResolutions.First();
+        }
     }
 }
